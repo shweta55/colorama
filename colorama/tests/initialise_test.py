381ff0606211ad08bff591384ce8d2eb68626b94
shweta55 <ss191619@gmail.com>
2020-01-10 12:18:18 +0530

    Update initialise_test.py

--- colorama/tests/initialise_test.py
+++ colorama/tests/initialise_test.py
@@ -37,13 +37,6 @@ class InitTest(TestCase):
         self.assertIs(sys.stdout, orig_stdout, 'stdout should not be wrapped')
         self.assertIs(sys.stderr, orig_stderr, 'stderr should not be wrapped')
 
-    @patch('colorama.initialise.reset_all')
-    @patch('colorama.ansitowin32.winapi_test', lambda *_: True)
-    def testInitWrapsOnWindows(self, _):
-        with osname("nt"):
-            init()
-            self.assertWrapped()
-
     def testInitDoesntWrapOnNonWindows(self):
         print("Inside testInitDoesntWrapOnNonWindows")
         with osname("posix"):
